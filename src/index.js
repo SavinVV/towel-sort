acded0a2e1b92c781a0114faaef0db239656b111
Vasiliy Savin <v.savin94@mail.ru>
2021-03-05 17:15:58 +0300

    fix: create functions towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (matrix === undefined) {
+    return [];
+  }
+  let result = [];
+  matrix.forEach((arr, i) => {
+      if (i % 2 == 0) {
+          arr.forEach((number) => {
+              result.push(number);
+          });
+      } else {
+          for (let n = arr.length - 1; n >= 0; n--) {
+              result.push(arr[n]);
+          }
+      }
+  });
+  return result;
+};
